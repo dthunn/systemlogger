a4f1847e3d732e1a6844acdf2c1b577f0d1ed42f
Dylan Thunn <dthunn90@gmail.com>
2019-10-16 08:20:24 -0500

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -14,6 +15,16 @@ app.use(express.json({ extended: false }));
 
 app.use('/api/techs', require('./routes/techs'));
 app.use('/api/logs', require('./routes/logs'));
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 4000;
 
